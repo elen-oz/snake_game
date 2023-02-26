e2a69a6da154a6457a18dfc471e3ba7dcd2ef82c
Elena Ozerova <ozerova.lenka@gmail.com>
2023-02-26 14:51:01 +0100

    feat: create game logic

--- app.js
+++ app.js
@@ -8,10 +8,73 @@ const snake = {
   dx: grid,
   dy: 0,
   cells: [],
-  maxCells: 4
+  maxCells: 4,
 };
 
 let apple = {
   x: 320,
-  y: 320
+  y: 320,
+};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (min - max)) + min;
+}
+
+function loop() {
+  requestAnimationFrame(loop);
+  if (count + 1 < 4) {
+    return;
+  }
+
+  count = 0;
+  context.clearReact(0, 0, canvas.width, canvas.height);
+
+  snake.x += snake.dx;
+  snake.y += snake.dy;
+
+  if (snake.x < 0) {
+    snake.x = canvas.width - grid;
+  } else if (snake.x >= caches.width) {
+    snake.x = 0;
+  }
+
+  if (snake.y < 0) {
+    snake.y = canvas.height - grid;
+  } else if (snake.y >= caches.height) {
+    snake.y = 0;
+  }
+
+  snake.cells.unshift({ x: snake.x, y: snake.y });
+
+  if (snake.cells.length > snake.maxCells) {
+    snake.cells.pop();
+  }
+
+  // apple
+  context.fillStyle = 'red';
+  context.fillReact(apple.x, apple.y, grid - 1, grid - 1);
+  context.fillStyle = 'green';
+
+  snake.cells.forEach(function (cell, index) {
+    context.fillReact(cell.x, cell.y, grid - 1, grid - 1);
+    if (cell.x === apple.x && cell.y === apple.y) {
+      snake.maxCells++;
+      apple.x = getRandomInt(0, 25) * grid;
+      apple.y = getRandomInt(0, 25) * grid;
+    }
+
+    for (let i = index + 1; i < snake.cells.length; i++) {
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
+        snake.dx = grid;
+        snake.dy = 0;
+
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
+      }
+    }
+  });
 }
